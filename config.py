1f698bdd3ab75206a7ed88f491cc2b815a00dfa5
rs <reissedler@gmail.com>
2019-05-29 19:09:44 +0100

    improvements

---
+++ config.py
@@ -0,0 +1,23 @@
+Config = {
+    'game': {
+        'caption': 'Snake.py',
+        'height': 500,
+        'width': 500,
+        'fps': 30,
+    },
+    'snake': {
+        'height': 20,
+        'width': 20,
+        'speed': 10
+    },
+    'apple': {
+        'width': 20,
+        'height': 20
+    },
+    'colors': {
+        'white': (255, 255, 255),
+        'black': (0, 0, 0),
+        'green': (0, 100, 0),
+        'red': (100, 0, 0)
+    }
+}
