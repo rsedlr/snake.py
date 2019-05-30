c7d8aed1beb69971f82f09fbdabbe694287c4829
MegaHertz3 <30870094+MegaHertz3@users.noreply.github.com>
2019-05-30 12:59:02 +0100

    improvements

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 Config = {
     'game': {
         'caption': 'Snake.py',
-        'height': 500,
-        'width': 500,
-        'fps': 30,
+        'height': 550,
+        'width': 550,
+        'fps': 15,  # 30
+        'bumper_size': 20
     },
     'snake': {
         'height': 20,
         'width': 20,
-        'speed': 10
+        'speed': 20  # 10
     },
     'apple': {
         'width': 20,
@@ -17,7 +18,7 @@ Config = {
     'colors': {
         'white': (255, 255, 255),
         'black': (0, 0, 0),
-        'green': (0, 100, 0),
-        'red': (100, 0, 0)
+        'green': (0, 200, 0),
+        'red': (200, 0, 0)
     }
 }
--- snake.py
+++ snake.py
@@ -1,15 +1,21 @@
-import pygame
+import pygame, random
 from config import Config
 
 class Game:
    def __init__(self, display):
       self.display = display
+      self.score = 0
         
    def loop(self):
       clock = pygame.time.Clock()
       snake = Snake(self.display)
+      apple = Apple(self.display)
       x_change = 0
       y_change = 0
+      bumper_x = Config['game']['width'] - Config['game']['bumper_size']
+      bumper_y = Config['game']['height'] - Config['game']['bumper_size']
+      pygame.font.init()
+      font = pygame.font.SysFont('Arial', 25, bold=True)
       while True:
          for event in pygame.event.get():
             print(event)
@@ -29,9 +35,36 @@ class Game:
                   x_change = 0
                   y_change = Config['snake']['speed']
         
-         self.display.fill(Config['colors']['black'])
+         self.display.fill(Config['colors']['green'])
+         pygame.draw.rect(
+            self.display, 
+            Config['colors']['black'], [
+               Config['game']['bumper_size'],
+               Config['game']['bumper_size'],
+               Config['game']['height'] - Config['game']['bumper_size'] * 2,
+               Config['game']['width'] - Config['game']['bumper_size'] * 2
+            ]
+         )
+         apple_rect = apple.draw()
          snake.move(x_change, y_change)
-         snake.draw()
+         score = font.render('Score: {}'.format(self.score), True, Config['colors']['white'])
+         score_rect = score.get_rect(
+            center=(Config['game']['width']/2, 
+                    Config['game']['height'] - Config['game']['bumper_size'] / 2)
+         )
+         self.display.blit(score, score_rect)
+         snake_rect = snake.draw()
+
+         if (snake.x_pos < 30 or snake.y_pos < 30 or 
+               snake.x_pos + Config['snake']['width'] > bumper_x or 
+               snake.y_pos + Config['snake']['height'] > bumper_y ):
+            self.score = 0
+            self.loop()
+
+         if apple_rect.colliderect(snake_rect):
+            apple.randomize()
+            self.score += 1
+
          pygame.display.update()
          clock.tick(Config['game']['fps'])
 
@@ -42,7 +75,7 @@ class Snake:
       self.display = display
 
    def draw(self):
-      pygame.draw.rect(
+      return pygame.draw.rect(
          self.display, 
          Config['colors']['green'],
          [
@@ -57,6 +90,35 @@ class Snake:
       self.x_pos += x_change
       self.y_pos += y_change
 
+
+class Apple:
+   def __init__(self, display):
+      self.x_pos = 0
+      self.y_pos = 0
+      self.display = display
+      self.randomize()
+
+   def randomize(self):
+      height = Config['game']['height']
+      width = Config['game']['width']
+      bumper = Config['game']['bumper_size']
+      max_x = (width - bumper - Config['snake']['width'])
+      max_y = (height - bumper - Config['snake']['height']) 
+      self.x_pos = random.randint(bumper, max_x)
+      self.y_pos = random.randint(bumper, max_y)
+         
+   def draw(self):
+      return pygame.draw.rect(
+         self.display, 
+         Config['colors']['red'],
+         [
+            self.x_pos,
+            self.y_pos,
+            Config['apple']['height'],
+            Config['apple']['width']
+         ]
+      )
+
 def main():
    display = pygame.display.set_mode(
       (Config['game']['width'], 
@@ -66,5 +128,7 @@ def main():
    game = Game(display)
    game.loop()
 
+
 if __name__ == '__main__':
    main()
+
